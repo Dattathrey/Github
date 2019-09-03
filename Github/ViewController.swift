ec183c18af87adb11b2b4bbbc2244450ecc30324
Biradar Dattathreya <dattabiradar357@achiever-96.local>
2019-09-03 12:13:27 +0530

    Initial Commit

---
+++ Github/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  Github
+//
+//  Created by Biradar Dattathreya on 9/3/19.
+//  Copyright © 2019 Biradar Dattathreya. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        print("sss")
+    }
+
+
+}
+
